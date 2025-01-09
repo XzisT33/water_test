300e6f874bbe92e9da7e2098460e8d8e8255b82b
XzisT <102463413+XzisT33@users.noreply.github.com>
2025-01-09 17:30:27 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+import pickle
+import pandas as pd
+from data_model import Water
+
+
+app = FastAPI(
+    title="Water Potability Prediction",
+    description="API to predict water potability",
+    version="0.1",
+)
+
+with open("D:\\MLOps\Data_Thinkers_MLOPS\\ml_pipeline\\rf_model.pkl", "rb") as file:
+    model = pickle.load(file) 
+
+@app.get("/")
+def index():
+    return "Welcome to the API"
+
+@app.post("/predict")
+def model_predict(water: Water):
+    sample = pd.DataFrame({
+        "ph": [water.ph],
+        "Hardness": [water.Hardness],
+        "Solids": [water.Solids],
+        "Chloramines": [water.Chloramines],
+        "Sulfate": [water.Sulfate],
+        "Conductivity": [water.Conductivity],
+        "Organic_carbon": [water.Organic_carbon],
+        "Trihalomethanes": [water.Trihalomethanes],
+        "Turbidity": [water.Turbidity]
+    })
+
+    pred = model.predict(sample)
+
+    if pred == 1:
+        return "Potable"
+    else:
+        return "Not Potable"
